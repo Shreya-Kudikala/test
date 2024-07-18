fba539a47cbc20032ae46437eb58a8088983a47f
Shreya Kudikala <shreya.kudikala@kyndryl.com>
2024-07-18 15:30:07 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Product struct {
+	ID    string `json:"id"`
+	Code  string `json:"code"`
+	Name  string `json:"name"`
+	Price string `json:"price"`
+}
+
+var products []Product
+
+func getProducts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(products)
+}
+
+func getProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range products {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var product Product
+	_ = json.NewDecoder(r.Body).Decode(&product)
+	product.ID = strconv.Itoa(rand.Intn(100000000))
+	products = append(products, product)
+	json.NewEncoder(w).Encode(product)
+}
+
+func updateProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range products {
+		if item.ID == params["id"] {
+			products = append(products[:index], products[index+1:]...)
+			var product Product
+			_ = json.NewDecoder(r.Body).Decode(&product)
+			product.ID = params["id"]
+			products = append(products, product)
+			json.NewEncoder(w).Encode(product)
+		}
+	}
+}
+
+func deleteProduct(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range products {
+		if item.ID == params["id"] {
+			products = append(products[:index], products[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(products)
+}
+
+func main() {
+	router := mux.NewRouter()
+	products = append(products, Product{ID: "1", Code: "486535", Name: "Apple iPhone 15", Price: "70999.00"})
+	products = append(products, Product{ID: "2", Code: "724928", Name: "HP Laptop", Price: "52025.75"})
+	products = append(products, Product{ID: "3", Code: "679646", Name: "Samsung Adaptor", Price: "1399"})
+	router.HandleFunc("/products", getProducts).Methods("GET")
+	router.HandleFunc("/products/{id}", getProduct).Methods("GET")
+	router.HandleFunc("/products", createProduct).Methods("POST")
+	router.HandleFunc("/products/{id}", updateProduct).Methods("PUT")
+	router.HandleFunc("/products/{id}", deleteProduct).Methods("DELETE")
+	fmt.Printf("Starting Server at Port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
